b6cfb9cab8db24a4729d5633e9dfd5521264a05a
Zoe <Zoe.urssu@gmail.com>
2023-12-31 03:45:03 +0900

    [Add/#3] DesignSystem Color

---
+++ HMH_iOS/HMH_iOS/Global/Resources/Color.swift
@@ -0,0 +1,69 @@
+//
+//  Color.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 12/31/23.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var background: UIColor {
+    return UIColor(red: 23.0 / 255.0, green: 23.0 / 255.0, blue: 27.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray7: UIColor {
+    return UIColor(red: 27.0 / 255.0, green: 27.0 / 255.0, blue: 35.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray6: UIColor {
+    return UIColor(red: 32.0 / 255.0, green: 32.0 / 255.0, blue: 40.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray5: UIColor {
+    return UIColor(red: 39.0 / 255.0, green: 41.0 / 255.0, blue: 52.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray4: UIColor {
+    return UIColor(red: 54.0 / 255.0, green: 57.0 / 255.0, blue: 72.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray3: UIColor {
+    return UIColor(red: 61.0 / 255.0, green: 63.0 / 255.0, blue: 77.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray2: UIColor {
+    return UIColor(red: 80.0 / 255.0, green: 80.0 / 255.0, blue: 94.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray1: UIColor {
+    return UIColor(red: 141.0 / 255.0, green: 141.0 / 255.0, blue: 159.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteText: UIColor {
+    return UIColor(red: 219.0 / 255.0, green: 218.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteBtn: UIColor {
+    return UIColor(red: 235.0 / 255.0, green: 236.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var bluePurpleButton: UIColor {
+    return UIColor(red: 61.0 / 255.0, green: 23.0 / 255.0, blue: 211.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var bluePurpleLine: UIColor {
+    return UIColor(red: 70.0 / 255.0, green: 30.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var bluePurpleProgress: UIColor {
+    return UIColor(red: 68.0 / 255.0, green: 40.0 / 255.0, blue: 236.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var bluePurpleActive: UIColor {
+    return UIColor(red: 43.0 / 255.0, green: 29.0 / 255.0, blue: 214.0 / 255.0, alpha: 0.22)
+  }
+
+}
+

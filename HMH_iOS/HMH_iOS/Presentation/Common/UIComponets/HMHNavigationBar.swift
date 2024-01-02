06536ea16a1415c9614dbaebba135315f16d090c
kim-seonwoo <sw204011@gmail.com>
2024-01-02 21:23:31 +0900

    [Feat/#7] Common - HMHNavigationBar 기능 구현

--- HMH_iOS/HMH_iOS/Presentation/Common/UIComponets/HMHNavigationBar.swift
+++ HMH_iOS/HMH_iOS/Presentation/Common/UIComponets/HMHNavigationBar.swift
@@ -13,34 +13,98 @@ import Then
 final class HMHNavigationBar: UIView {
     @frozen
     enum NavigationBarType {
-        case onBoarding
+        case normal
         case logo
-        case backButton
     }
     
     private weak var viewController: UIViewController?
     private let type: NavigationBarType
-
-    init(_ viewController: UIViewController, leftItem type: NavigationBarType) {
+    
+    private lazy var backArrowButton = UIButton().then {
+        $0.backgroundColor = .blue
+        $0.addTarget(self, action: #selector(backArrowButtonTapped), for: .touchUpInside)
+        $0.isHidden = true
+    }
+    
+    private lazy var pointImageView = UIImageView().then {
+        $0.backgroundColor = .blue
+        $0.isHidden = true
+    }
+    
+    private lazy var logoImageView = UIImageView().then {
+        $0.backgroundColor = .blue
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        $0.isHidden = true
+    }
+    
+    init(_ viewController: UIViewController, leftItem type: NavigationBarType, isBackButton: Bool, isTitleLabel: Bool, isPointImage: Bool, titleText: String? = nil) {
         self.viewController = viewController
         self.type = type
         super.init(frame: .zero)
+        
+        backArrowButton.isHidden = !isBackButton
+        titleLabel.isHidden = !isTitleLabel
+        pointImageView.isHidden = !isPointImage
+        titleLabel.text = titleText
+        
         setUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setUI() {
+    private func setUI() {
         setHierarchy()
         setConstraints()
     }
-
-    func setHierarchy() {
+    
+    private func setHierarchy() {
+        switch type {
+        case .normal:
+            self.addSubviews(backArrowButton, titleLabel, pointImageView)
+            
+        case .logo:
+            self.addSubviews(logoImageView)
+        }
     }
-
-    func setConstraints() {
+    
+    private func setConstraints() {
+        switch type {
+        case .normal:
+            backArrowButton.snp.makeConstraints {
+                $0.top.equalToSuperview().offset(20)
+                $0.leading.equalToSuperview().inset(20)
+                $0.size.equalTo(24)
+            }
+            
+            titleLabel.snp.makeConstraints {
+                $0.centerX.centerY.equalToSuperview()
+            }
+            
+            pointImageView.snp.makeConstraints {
+                $0.top.equalToSuperview().offset(20)
+                $0.trailing.equalToSuperview().inset(20)
+                $0.size.equalTo(24)
+            }
+            
+        case .logo:
+            logoImageView.snp.makeConstraints {
+                $0.top.equalToSuperview().offset(20)
+                $0.leading.equalToSuperview().inset(20)
+                $0.size.equalTo(24)
+            }
+        }
     }
 }
 
+
+extension HMHNavigationBar {
+    @objc private func backArrowButtonTapped() {
+        self.viewController?.navigationController?.popViewController(animated: true)
+    }
+}

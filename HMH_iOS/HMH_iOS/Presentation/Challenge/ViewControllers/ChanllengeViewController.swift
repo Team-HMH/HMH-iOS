bf4610aab3673c93b7d4e894faf99cab21dccb51
boyeon <judy010119@gmail.com>
2024-01-03 03:06:36 +0900

    [Feat/#6] Presentation - ViewControllers 배경색 변경

--- HMH_iOS/HMH_iOS/Presentation/Challenge/ViewControllers/ChanllengeViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/ViewControllers/ChanllengeViewController.swift
@@ -5,4 +5,14 @@
 //  Created by 김보연 on 1/3/24.
 //
 
-import Foundation
+import UIKit
+
+final class ChanllengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
--- HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -5,4 +5,12 @@
 //  Created by 김보연 on 1/3/24.
 //
 
-import Foundation
+import UIKit
+
+final class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}

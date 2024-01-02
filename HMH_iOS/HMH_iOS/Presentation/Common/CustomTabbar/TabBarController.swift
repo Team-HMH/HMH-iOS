c1d73c3ca26cad39189b105147cf8c4445d62ef4
boyeon <judy010119@gmail.com>
2024-01-03 02:39:04 +0900

    [Set/#6] Common - TabBarController 파일 추가

---
+++ HMH_iOS/HMH_iOS/Presentation/Common/CustomTabbar/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  HMH_iOS
+//
+//  Created by 김보연 on 1/3/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

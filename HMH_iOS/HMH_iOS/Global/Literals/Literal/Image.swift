eec75ecbdede673fd6e27abe8f9d3e7259b38e58
kim-seonwoo <sw204011@gmail.com>
2024-01-02 21:49:57 +0900

    [Add/#7] Literal - ImageLiterals 파일 추가

--- HMH_iOS/HMH_iOS/Global/Literals/Literal/Image.swift
+++ HMH_iOS/HMH_iOS/Global/Literals/Literal/Image.swift
@@ -5,4 +5,26 @@
 //  Created by Seonwoo Kim on 1/2/24.
 //
 
-import Foundation
+import UIKit
+
+enum ImageLiterals {
+    enum NavigationBar {
+        static var icArrowLeft: UIImage { .load(named: "ic_back")}
+    }
+}
+
+extension UIImage {
+    static func load(named imageName: String) -> UIImage {
+        guard let image = UIImage(named: imageName, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        return image
+    }
+    
+    func resize(to size: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}

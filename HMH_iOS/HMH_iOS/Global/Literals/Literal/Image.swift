2d42aad4a5cb32fdc1a31568d6bf5e794f766327
kim-seonwoo <sw204011@gmail.com>
2024-01-02 21:49:24 +0900

    [Add/#7] Assets - icLeft asset 추가

---
+++ HMH_iOS/HMH_iOS/Global/Literals/Literal/Image.swift
@@ -0,0 +1,8 @@
+//
+//  Image.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 1/2/24.
+//
+
+import Foundation
--- HMH_iOS/HMH_iOS/Global/Literals/Literal/Literal.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Literal.swift
-//  HMH_iOS
-//
-//  Created by 지희의 MAC on 12/23/23.
-//
-
-import Foundation

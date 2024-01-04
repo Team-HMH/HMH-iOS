a7d16d8502c2c39bca5a6df1aab0577246755d8c
Zoe <Zoe.urssu@gmail.com>
2024-01-05 02:27:44 +0900

    [Feat/#14] 파일 분리

--- HMH_iOS/HMH_iOS/Global/Literals/Literal/Image.swift
+++ HMH_iOS/HMH_iOS/Global/Literals/Literal/Image.swift
@@ -22,18 +22,3 @@ enum ImageLiterals {
     }
 }
 
-extension UIImage {
-    static func load(named imageName: String) -> UIImage {
-        guard let image = UIImage(named: imageName, in: nil, compatibleWith: nil) else {
-            return UIImage()
-        }
-        return image
-    }
-    
-    func resize(to size: CGSize) -> UIImage {
-        let image = UIGraphicsImageRenderer(size: size).image { _ in
-            draw(in: CGRect(origin: .zero, size: size))
-        }
-        return image
-    }
-}

fac681895c166359f52415c48b923b4da87f390f
Zoe <Zoe.urssu@gmail.com>
2024-01-05 02:27:44 +0900

    [Feat/#14] 파일 분리

---
+++ HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
@@ -0,0 +1,16 @@
+//
+//  String.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 1/5/24.
+//
+
+import Foundation
+
+enum StringLiteral {
+    enum TabBar {
+        static var challengeTitle = "챌린지"
+        static var homeTitle = "홈"
+        static var myPageTitle = "마이페이지"
+    }
+}

ca0e6f5c796e865824c5d8a18fc87196e036b29d
Zoe <Zoe.urssu@gmail.com>
2024-01-05 02:54:12 +0900

    [Add/#14] toast message Extension 추가

--- HMH_iOS/HMH_iOS/Global/Extension/UIView+.swift
+++ HMH_iOS/HMH_iOS/Global/Extension/UIView+.swift
@@ -29,4 +29,33 @@ extension UIView {
         layer.borderWidth = width
         layer.borderColor = color.cgColor
     }
+    
+    // 토스트 메세지
+    func showToast(message: String, at: CGFloat = 17) {
+        let toastLabel = UILabel()
+        toastLabel.backgroundColor = .gray8
+        toastLabel.textColor = .whiteText
+        toastLabel.textAlignment = .center
+        toastLabel.font = .iosDetail4Medium12
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 8
+        toastLabel.clipsToBounds = true
+        
+        let toastWidth = 248.adjusted
+        let toastHeight = 43.adjusted
+        toastLabel.frame = CGRect(x: self.frame.size.width / 2 - toastWidth / 2,
+                                  y: self.frame.size.height - toastHeight - at.adjusted,
+                                  width: toastWidth,
+                                  height: toastHeight)
+        if let window = UIApplication.shared.keyWindow {
+            window.addSubview(toastLabel)
+        }
+        
+        UIView.animate(withDuration: 3.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
 }

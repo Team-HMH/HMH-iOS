c6ba8e7aafe7325ddc2cafe17f15f4eed600146e
Zoe <Zoe.urssu@gmail.com>
2024-01-06 01:41:55 +0900

    [Feat/#19] Challenge - CollectionView Cell

--- HMH_iOS/HMH_iOS/Presentation/Challenge/Cells/AppListCollectionViewCell.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Cells/AppListCollectionViewCell.swift
@@ -9,4 +9,69 @@ import UIKit
 
 class AppListCollectionViewCell: UICollectionViewCell {
     
+    static let identifer = "AppListCollectionViewCell"
+    
+    let appImageView = UIImageView().then {
+        $0.backgroundColor = .blue
+        $0.makeCornerRound(radius: 8)
+    }
+    
+    let appIconLabel = UILabel().then {
+        $0.text = "instagram"
+        $0.textColor = .systemGray4
+        $0.font = .systemFont(ofSize: 14)
+    }
+    
+    let timeLabel = UILabel().then {
+        $0.text = "1시간 30분"
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 16)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
+    }
+    
+    private func setUI(){
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    private func setViewHierarchy() {
+        self.backgroundColor = .clear
+        contentView.backgroundColor = .systemGray
+        contentView.makeCornerRound(radius: 6)
+        contentView.addSubviews(backView,appImageView, appIconLabel, timeLabel)
+    }
+    
+    private func setConstraints() {
+        
+        appImageView.snp.makeConstraints {
+            $0.size.equalTo(40)
+            $0.leading.equalToSuperview().offset(17)
+            $0.centerY.equalToSuperview()
+        }
+        
+        appIconLabel.snp.makeConstraints {
+            $0.leading.equalTo(appImageView.snp.trailing).offset(14)
+            $0.centerY.equalToSuperview()
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(27)
+            $0.centerY.equalToSuperview()
+        }
+        
+    }
+
 }
--- HMH_iOS/HMH_iOS/Presentation/Challenge/Cells/DateCollectionViewCell.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Cells/DateCollectionViewCell.swift
@@ -7,6 +7,50 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class DateCollectionViewCell: UICollectionViewCell {
+    static let identifer = "DateCollectionViewCell"
     
+    let dateLabel = UILabel().then {
+        $0.text = "1"
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 14, weight: .medium)
+    }
+    
+    let imageView = UIImageView().then {
+        $0.backgroundColor = .gray
+        $0.makeCornerRound(radius: 8)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setUI(){
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    private func setViewHierarchy() {
+        contentView.addSubviews(dateLabel, imageView)
+    }
+    
+    private func setConstraints() {
+        dateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(6)
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(40)
+        }
+    }
 }

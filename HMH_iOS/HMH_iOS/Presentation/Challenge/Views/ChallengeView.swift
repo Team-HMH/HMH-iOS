7c34cecbade7a59ce3fbfd9bd9c3263a7654c3db
Zoe <Zoe.urssu@gmail.com>
2024-01-06 00:39:51 +0900

    [Add/#19] Challenge - Add File

---
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/ChallengeView.swift
@@ -0,0 +1,41 @@
+//
+//  ChallengeView.swift
+//  HMH_iOS
+//
+//  Created by 지희의 MAC on 1/6/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChallengeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        addTarget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+    func addTarget() {
+        
+    }
+}

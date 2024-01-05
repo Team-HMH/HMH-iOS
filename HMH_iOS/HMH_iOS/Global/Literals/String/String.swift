4d3278e87a202cb026060562574ec4215d0ef687
boyeon <judy010119@gmail.com>
2024-01-06 01:27:10 +0900

    [Add/#18] Literals - Title과 Description 추가

--- HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
+++ HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
@@ -19,4 +19,13 @@ enum StringLiteral {
         static let cancel = "취소"
         static let close = "닫기"
     }
+    
+    enum AlertTitle {
+        static let logout = "HMH을 로그아웃 하실 건가요?"
+        static let quit = "HMH을 탈퇴 하실 건가요?"
+    }
+    
+    enum AlertDescription {
+        static let quit = "회원탈퇴 후 유저의 정보는 30일 동안 임시보관 후 영구 삭제됩니다."
+    }
 }
---
+++ HMH_iOS/HMH_iOS/Presentation/Common/CustomAlert/CustomTitleAlert.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTitleAlert.swift
+//  HMH_iOS
+//
+//  Created by 김보연 on 1/6/24.
+//
+
+import Foundation

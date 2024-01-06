883a888089363d5dd9e4a25226c42d9b7ae6553a
boyeon <judy010119@gmail.com>
2024-01-07 02:29:06 +0900

    [Feat/#18] Protocol - AlertDelegate 추가

---
+++ HMH_iOS/HMH_iOS/Global/Protocols/AlertDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AlertDelegate.swift
+//  HMH_iOS
+//
+//  Created by 김보연 on 1/7/24.
+//
+
+import Foundation
+
+protocol AlertDelegate: AnyObject {
+    func enabledButtonTapped()
+    func alertDismissTapped()
+}

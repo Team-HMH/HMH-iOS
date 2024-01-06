d87e5f47dec7194281189ebccf4b925cbe8897f6
boyeon <judy010119@gmail.com>
2024-01-07 02:31:31 +0900

    [Feat/#18] CustomAlert - AlertViewController 추가

--- HMH_iOS/HMH_iOS/Presentation/Common/CustomAlert/AlertViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/Common/CustomAlert/AlertViewController.swift
@@ -7,23 +7,88 @@
 
 import UIKit
 
-class AlertViewController: UIViewController {
+enum AlertType {
+    case HMHLogoutAlert
+    case HMHQuitALert
+}
 
+final class AlertViewController: UIViewController {
+    var alertType: AlertType?
+    var okAction: (() -> Void)?
+    
+    private let logoutAlert = HMHLogoutAlert()
+    private let quitAlert = HMHQuitAlert()    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black.withAlphaComponent(0.7)
+        
+        setDelegate()
+        setAlertType()
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUI() {
+        setHierarchy()
+        setConstraint()
     }
-    */
+    
+    private func setHierarchy() {
+        view.addSubviews(logoutAlert, quitAlert)
+    }
+    
+    private func setConstraint() {
+        logoutAlert.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(177.adjusted)
+            $0.width.equalTo(293.adjusted)
+        }
+        
+        quitAlert.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(203.adjusted)
+            $0.width.equalTo(293.adjusted)
+        }
+    }
+    
+    private func setAlertType() {
+        switch alertType {
+        case .HMHLogoutAlert:
+            setAlertView(logout: true, quit: false)
+        case .HMHQuitALert:
+            setAlertView(logout: false, quit: true)
+        default:
+            break
+        }
+    }
+    
+    private func setAlertView(logout: Bool, quit: Bool) {
+        logoutAlert.isHidden = !logout
+        quitAlert.isHidden = !quit
+    }
+    
+    func setDelegate() {
+        logoutAlert.delegate = self
+        quitAlert.delegate = self
+    }
+    
+    func emptyActions() {
+        
+    }
+    
+    func setAlertType(_ type: AlertType) {
+        self.alertType = type
+    }
+}
 
+extension AlertViewController: AlertDelegate {
+    func enabledButtonTapped() {
+        dismiss(animated: false) {
+            (self.okAction ?? self.emptyActions)()
+        }
+    }
+    
+    func alertDismissTapped() {
+        dismiss(animated: true)
+    }
 }

7ec56261681a589e25cd7551fa20a207da3cd8bb
boyeon <judy010119@gmail.com>
2024-01-09 03:38:50 +0900

    [Feat/#31] MyPageViewController - myPageViewController 추가

--- HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -8,16 +8,13 @@
 import UIKit
 
 final class MyPageViewController: UIViewController {
-    private let navigationBar = HMHNavigationBar(leftItem: .normal, isBackButton: false, isTitleLabel: true, isPointImage: false, isBackGroundGray: true, titleText: "마이페이지")
+    private let navigationBar = HMHNavigationBar(leftItem: .normal, isBackButton: false, isTitleLabel: true, isPointImage: false, isBackGroundGray: false, titleText: "마이페이지")
     private let myPageView = MyPageView()
-    
-    override func loadView() {
-        self.view = myPageView
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        configureView()
     }
     
     private func setUI() {
@@ -27,12 +24,21 @@ final class MyPageViewController: UIViewController {
     }
     
     private func setHierarchy() {
-        view.addSubviews(navigationBar)
+        view.addSubviews(navigationBar, myPageView)
     }
     
     private func setConstraints() {
         navigationBar.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
         }
+        
+        myPageView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.bottom.horizontalEdges.equalToSuperview()
+        }
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .gray8
     }
 }

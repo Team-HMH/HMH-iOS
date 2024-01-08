9710d4271dbdf647941f454812f16bc2ef20906c
boyeon <judy010119@gmail.com>
2024-01-09 00:08:22 +0900

    [Feat/#31] MyPage - 네비바 및 myPageView 연결

--- HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 final class MyPageViewController: UIViewController {
+    private let navigationBar = HMHNavigationBar(leftItem: .normal, isBackButton: false, isTitleLabel: true, isPointImage: false, isBackGroundGray: true, titleText: "마이페이지")
+    private let myPageView = MyPageView()
+    
+    override func loadView() {
+        self.view = myPageView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        setUI()
+    }
+    
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+        
+    }
+    
+    private func setHierarchy() {
+        view.addSubviews(navigationBar)
+    }
+    
+    private func setConstraints() {
+        navigationBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
     }
 }

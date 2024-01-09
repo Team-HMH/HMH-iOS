aa2551e1147474c64c3a21bdddf327e4964331fe
kim-seonwoo <sw204011@gmail.com>
2024-01-10 00:41:16 +0900

    [Add/#42] CreateChallenge - 챌린지 생성 뷰컨트롤러 추가

--- HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
+++ HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
@@ -75,10 +75,12 @@ enum StringLiteral {
     enum OnboardigMain {
         static let timeSurvey = "하루 평균 휴대폰을\n얼마나 사용하나요?"
         static let problemSurvey = "휴대폰을 사용할 때\n어떤 문제를 겪고 있나요?"
+        static let selectPeriod = "챌린지 기간을 선택해 주세요"
     }
     
     enum OnboardigSub {
         static let problemSurvey = "해당 문항은 최대 2개까지 선택할 수 있어요"
+        static let selectPeriod = "첫 챌린지로 가볍게 도전하기 좋은 7일을 추천해요"
     }
     
     enum TimeSurveySelect {
@@ -93,5 +95,12 @@ enum StringLiteral {
         static let secondSelect = "이용 시간이 스스로 제어되지 않아요"
         static let thirdSelect = "특정 앱에 수시로 접속하게 돼요"
         static let fourthSelect = "중독을 탈출하려고 노력해도 잘 안 돼요"
+    }
+    
+    enum PeriodSelect {
+        static let firstSelect = "7일"
+        static let secondSelect = "14일"
+        static let thirdSelect = "20일"
+        static let fourthSelect = "30일"
     }
 }
---
+++ HMH_iOS/HMH_iOS/Presentation/CreateChallenge/ViewControllers/ApprovePermisionController.swift
@@ -0,0 +1,71 @@
+//
+//  ApprovePermisionController.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 1/10/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ApprovePermisionController: OnboardingBaseViewController {
+    private let surveyView = SurveyView(buttonType: .solitary)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        configureSurveyView()
+        setDelegate()
+        setTimeSurvey()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setUI()
+    }
+    
+    private func setUI(){
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(surveyView)
+    }
+    
+    private func setConstraints() {
+        surveyView.snp.makeConstraints {
+            $0.top.equalTo(mainTitleLabel.snp.bottom).offset(94.adjustedHeight)
+            $0.leading.trailing.equalTo(progressBar)
+        }
+    }
+    
+    private func setDelegate() {
+        self.delegate = self
+    }
+    
+    private func setTimeSurvey() {
+        view.backgroundColor = .background
+        step = 5
+    }
+    
+    private func configureSurveyView() {
+        nextButtonText = StringLiteral.OnboardingButton.next
+        nextButton.setButtonText(buttonTitle: nextButtonText)
+        mainTitleText = StringLiteral.OnboardigMain.selectPeriod
+        subTitleText = StringLiteral.OnboardigSub.selectPeriod
+        surveyView.firstButton.setButtonText(buttonTitle: StringLiteral.PeriodSelect.firstSelect)
+        surveyView.secondButton.setButtonText(buttonTitle: StringLiteral.PeriodSelect.secondSelect)
+        surveyView.thirdButton.setButtonText(buttonTitle: StringLiteral.PeriodSelect.thirdSelect)
+        surveyView.fourthButton.setButtonText(buttonTitle: StringLiteral.PeriodSelect.fourthSelect)
+    }
+}
+
+extension ApprovePermisionController: NextViewPushDelegate {
+    func didTapButton() {
+        let nextViewController = SelectTotalTimeController()
+        self.navigationController?.pushViewController(nextViewController, animated: false)
+    }
+}
+

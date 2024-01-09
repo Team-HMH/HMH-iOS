77c69afbacdc75e7f78be64763cf4dd26cab337d
boyeon <judy010119@gmail.com>
2024-01-09 19:45:19 +0900

    [Feat/#31] MyPageCell - infoHeaderView 구현

--- HMH_iOS/HMH_iOS/Presentation/MyPage/Cells/InfoHeaderView.swift
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/Cells/InfoHeaderView.swift
@@ -7,6 +7,46 @@
 
 import UIKit
 
-class InfoHeaderView: UICollectionReusableView {
-        
+import SnapKit
+import Then
+
+final class InfoHeaderView: UICollectionReusableView {
+    
+    static let identifier = "InfoHeaderView"
+    
+    private let infoLabel = UILabel().then {
+        $0.font = .iosText4Semibold16
+        $0.textColor = .gray2
+        $0.text = StringLiteral.myPage.info
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        self.addSubview(infoLabel)
+    }
+    
+    private func setConstraints() {
+        infoLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-11.adjusted)
+            $0.leading.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func configureView() {
+        self.backgroundColor = .background
+    }
 }

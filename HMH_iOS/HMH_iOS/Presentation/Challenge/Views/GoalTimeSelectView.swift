5ed34eaa49788177e18f37bfe3a8f2f296011ca1
Zoe <Zoe.urssu@gmail.com>
2024-01-10 04:25:57 +0900

    [Feat/#36] GoalTime - 목표 시간 설정 뷰 구현

--- HMH_iOS/HMH_iOS/Presentation/Challenge/Views/GoalTimeSelectView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/GoalTimeSelectView.swift
@@ -5,4 +5,77 @@
 //  Created by 지희의 MAC on 1/9/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+import FamilyControls
+
+class GoalTimeSelectView: UIView {
+    
+    var selectedValue = FamilyActivitySelection()
+    
+    private let titlelabel = UILabel().then {
+        $0.font = .iosTitle1Semibold22
+        $0.textColor = .whiteText
+        $0.text = StringLiteral.Challenge.GoalTime.titleText
+        $0.numberOfLines = 2
+    }
+    private let subTitlelabel = UILabel().then {
+        $0.font = .iosDetail1Regular14
+        $0.textColor = .whiteText
+        $0.text = StringLiteral.Challenge.GoalTime.titleText
+        $0.numberOfLines = 2
+    }
+    private let picker = SpecificTimePickerView()
+    private let progressBar = ProgressBarManager()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        configureView()
+        progressBar.updateProgress(step: 6)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setUI(){
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(titlelabel, subTitlelabel, progressBar.progressBarView, picker)
+    }
+    
+    private func setConstraints() {
+        
+        progressBar.progressBarView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(62)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        titlelabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(145.adjusted)
+            $0.leading.equalToSuperview().offset(21.adjusted)
+        }
+        
+        subTitlelabel.snp.makeConstraints {
+            $0.top.equalTo(titlelabel.snp.bottom).offset(7)
+            $0.leading.equalTo(titlelabel.snp.leading)
+        }
+        
+        picker.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(335.adjusted)
+        }
+    }
+    
+    private func configureView() {
+        self.backgroundColor = .background
+        
+    }
+    
+}

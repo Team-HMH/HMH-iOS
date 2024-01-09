a0e9123092d318c0f5ab51e02064dc7a875a5fcb
boyeon <judy010119@gmail.com>
2024-01-09 18:56:26 +0900

    [Add/#31] MyPage - Add footer to collectionView

--- HMH_iOS/HMH_iOS/Presentation/MyPage/Views/MyPageView.swift
+++ HMH_iOS/HMH_iOS/Presentation/MyPage/Views/MyPageView.swift
@@ -17,27 +17,150 @@ final class MyPageView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setUI()
+        setRegister()
+        configreCollectionView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setUI() {
+    private func setUI() {
         setHierarchy()
         setConstraints()
     }
     
-    func setHierarchy() {
+    private func setHierarchy() {
         self.addSubviews(myPageCollectionView)
-        
     }
     
-    func setConstraints() {
+    private func setConstraints() {
         myPageCollectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+    }
+    
+    private func setRegister() {
+        myPageCollectionView.register(UserPointHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: UserPointHeaderView.identifier)
+        myPageCollectionView.register(MypageCollectionViewCell.self, forCellWithReuseIdentifier: MypageCollectionViewCell.identifier)
+        myPageCollectionView.register(LogoutAndQuitFooterView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: LogoutAndQuitFooterView.identifier)
+    }
+    
+    private func configreCollectionView() {
+        myPageCollectionView.delegate = self
+        myPageCollectionView.dataSource = self
+    }
+}
+
+
+extension MyPageView: UICollectionViewDelegate {}
+
+extension MyPageView: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 2
+        case 1:
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ listCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = myPageCollectionView.dequeueReusableCell(withReuseIdentifier: MypageCollectionViewCell.identifier, for: indexPath) as? MypageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        switch indexPath.section {
+        case 0:
+            if indexPath.item == 0 {
+                item.configureCell(iconImage: ImageLiterals.myPage.icJourney, labelText: StringLiteral.myPage.journey, labelColor: .gray5)
+            } else if indexPath.item == 1 {
+                item.configureCell(iconImage: ImageLiterals.myPage.icStore, labelText: StringLiteral.myPage.store, labelColor: .gray5)
+            }
+            return item
+            
+        case 1:
+            if indexPath.item == 0 {
+                item.configureCell(iconImage: nil, labelText: StringLiteral.myPage.TOS, labelColor: .whiteText)
+            } else if indexPath.item == 1 {
+                item.configureCell(iconImage: nil, labelText: StringLiteral.myPage.privacyPolicy, labelColor: .whiteText)
+            }
+            return item
+            
+        default:
+            return UICollectionViewCell()
+        }
+    }
+}
+
+extension MyPageView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.section {
+        default:
+            let itemWidthOrHeight: CGFloat = 60
+            return CGSize(width: collectionView.frame.width.adjustedWidth, height: itemWidthOrHeight.adjustedHeight)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let sectionInsets = UIEdgeInsets.zero
+        switch section {
+        case 0:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+        case 1:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        default:
+            break
+        }
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch indexPath.section {
+        case 0:
+            if kind == UICollectionView.elementKindSectionHeader {
+                guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: UserPointHeaderView.identifier, for: indexPath) as? UserPointHeaderView else {
+                    return UICollectionReusableView()
+                }
+                return headerView
+            }
+        case 1:
+            if kind == UICollectionView.elementKindSectionFooter {
+                guard let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: LogoutAndQuitFooterView.identifier, for: indexPath) as? LogoutAndQuitFooterView else {
+                    return UICollectionReusableView()
+                }
+                return footerView
+            }
+        default:
+            return UICollectionReusableView()
+        }
+        return UICollectionReusableView()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return .init(width: UIScreen.main.bounds.width, height: 205.adjusted)
+        } else {
+            return .zero
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if section == 1 {
+            return .init(width: UIScreen.main.bounds.width, height: 96.adjusted)
+        } else {
+            return .zero
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
     }
 }

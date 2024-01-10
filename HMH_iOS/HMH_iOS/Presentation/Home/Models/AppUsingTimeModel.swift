2c76f9b5e554fb2e51d95f41f859e2a0f6436195
boyeon <judy010119@gmail.com>
2024-01-11 05:51:09 +0900

    [Feat/#44] HomeModel - 시간 더미데이터 구축

--- HMH_iOS/HMH_iOS/Presentation/Home/Models/AppUsingTimeModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Models/AppUsingTimeModel.swift
@@ -5,4 +5,37 @@
 //  Created by 김보연 on 1/10/24.
 //
 
-import Foundation
+import UIKit
+
+struct AppUsingTimeModel {
+    
+    let appGoalTime: Int
+    let usingAppIcon: UIImage
+    let usingAppName: String
+    let usedTime: Int
+    
+    init(appGoalTime: Int, usingAppIcon: UIImage, usingAppName: String, usedTime: Int) {
+        self.appGoalTime = appGoalTime
+        self.usingAppIcon = usingAppIcon
+        self.usingAppName = usingAppName
+        self.usedTime = usedTime
+    }
+}
+
+var appUsingTimeModel: [AppUsingTimeModel] = [
+    
+    .init(appGoalTime: 7200000,
+          usingAppIcon: ImageLiterals.TabBar.icHomeSelected,
+          usingAppName: "Instagram",
+          usedTime: 3200000),
+    
+        .init(appGoalTime: 2700000,
+              usingAppIcon: ImageLiterals.TabBar.icChallengeSelected,
+              usingAppName: "melon",
+              usedTime: 1500000),
+    
+        .init(appGoalTime: 9000000,
+              usingAppIcon: ImageLiterals.TabBar.icMyPageSelected,
+              usingAppName: "Netflix",
+              usedTime: 7900000),
+]

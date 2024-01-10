c9a4d156e58d8b82257d914aedb0f214a29e89a6
boyeon <judy010119@gmail.com>
2024-01-11 05:52:03 +0900

    [Feat/#44] HomeCell - 블랙홀 이미지 셀 구현

--- HMH_iOS/HMH_iOS/Presentation/Home/Cells/BlackHoleImageCell.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Cells/BlackHoleImageCell.swift
@@ -7,6 +7,51 @@
 
 import UIKit
 
-class BlackHoleImageCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class BlackHoleImageCell: UICollectionViewCell {
     
+    static let identifier = "BlackHoleImageCell"
+    
+    private let blackHoleImageView = UIImageView().then {
+        $0.image = ImageLiterals.TabBar.icHome
+        $0.alpha = 0.1
+    }
+    private let homeBlackHoleStateLabel = UILabel().then {
+        $0.font = .iosTitle3Semibold22
+        $0.textColor = .whiteText
+        $0.setTextWithLineHeight(text: StringLiteral.Home.blackHoleState, lineHeight: 33)
+        $0.numberOfLines = 2
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(blackHoleImageView, homeBlackHoleStateLabel)
+    }
+    
+    private func setConstraints() {
+        blackHoleImageView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(blackHoleImageView.snp.width)
+        }
+        
+        homeBlackHoleStateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(23.adjusted)
+            $0.leading.equalToSuperview().inset(21.adjusted)
+        }
+    }
 }

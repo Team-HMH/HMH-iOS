b5f690c1e47365c41e11f721874d98b7924c7253
kim-seonwoo <sw204011@gmail.com>
2024-01-12 22:35:06 +0900

    [Add/#46] UserDefaultWrapper 추가

---
+++ HMH_iOS/HMH_iOS/Global/SupportingFiles/User/UserDefaultWrapper.swift
@@ -0,0 +1,30 @@
+//
+//  UserDefaultWrapper.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 1/12/24.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefaultWrapper<T> {
+    
+    var wrappedValue: T? {
+        get {
+            return UserDefaults.standard.object(forKey: self.key) as? T
+        }
+        
+        set {
+            if newValue == nil {
+                UserDefaults.standard.removeObject(forKey: key)
+            } else { UserDefaults.standard.setValue(newValue, forKey: key) }
+        }
+    }
+    
+    private let key: String
+    
+    init(key: String) {
+        self.key = key
+    }
+}

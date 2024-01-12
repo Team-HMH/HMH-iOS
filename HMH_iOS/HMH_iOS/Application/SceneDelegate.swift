9160d14a349cc12e7921b2d998968924e383354b
kim-seonwoo <sw204011@gmail.com>
2024-01-12 22:42:57 +0900

    [Feat/#46] SceneDelegate - 애플 로그인 유저 상태 조회 로직 추가

--- HMH_iOS/HMH_iOS/Application/SceneDelegate.swift
+++ HMH_iOS/HMH_iOS/Application/SceneDelegate.swift
@@ -6,11 +6,27 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    
+    //
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: "000340.8d411a5d11d84e8da0c22ef43dce465a.1657" ?? "") { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                print("authorized")
+            case .revoked:
+                print("revoked")
+            case .notFound:
+                print("notFound")
+            default:
+                break
+            }
+        }
+    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -24,7 +40,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             DispatchQueue.main.async{
-                showTabBarViewController()
+                if UserManager.shared.hasAccessToken {
+                    //애플 로그인 토큰 리프레쉬 로직. 성공한다면 엑세스와 리프레시 토큰 업데이트 그러면서 이동
+                    // 토큰 리프레시가 실패 한다. 로그인 컨트롤러로 바로 이동.
+//                    UserManager.shared.updateToken(,)
+                    
+                    showTabBarViewController()
+                } else {
+                    showLoginViewController()
+                }
             }
         }
         

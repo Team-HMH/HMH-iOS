456e0adf105112c81001aeaeb9fbf0fd3a83e1b1
Zoe <Zoe.urssu@gmail.com>
2024-01-14 06:53:36 +0900

    [Feat/#70] 스크린타임 권한 허용 뷰

--- HMH_iOS/HMH_iOS/Presentation/CreateChallenge/ViewControllers/ApprovePermisionController.swift
+++ HMH_iOS/HMH_iOS/Presentation/CreateChallenge/ViewControllers/ApprovePermisionController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 import SnapKit
 import Then
+import FamilyControls
 
 final class ApprovePermisionController: OnboardingBaseViewController {
+    private let authorizationCenter = AuthorizationCenter.shared
+    let userNotiCenter = UNUserNotificationCenter.current()
+    var isApproveScreenTime = false
+    var isApproveNoti = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSurveyView()
         setDelegate()
+        nextButton.updateStatus(isEnabled: true)
         setTimeSurvey()
     }
     
@@ -34,12 +41,54 @@ final class ApprovePermisionController: OnboardingBaseViewController {
         mainTitleText = StringLiteral.OnboardigMain.approvePermision
         subTitleText = StringLiteral.OnboardigSub.approvePermision
     }
+    private func requestPermission(completion: @escaping (Bool) -> Void) {
+        switch authorizationCenter.authorizationStatus {
+        case .notDetermined:
+            Task {
+                do {
+                    try await authorizationCenter.requestAuthorization(for: .individual)
+                    if authorizationCenter.authorizationStatus == .approved {
+                        completion(true)
+                    } else {
+                        completion(false)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                    completion(false)
+                }
+            }
+        case .denied:
+            completion(false)
+        case .approved:
+            completion(true)
+        @unknown default:
+            self.view.showToast(message: "알 수 없는 에러가 발생했습니다.", at: 20)
+            completion(false)
+        }
+    }
+    
+    func requestAuthNoti() {
+        userNotiCenter.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { didAllow, error in
+            if didAllow {
+                self.isApproveNoti = true
+            } else {
+                self.isApproveNoti = false
+            }
+        })
+    }
 }
 
 extension ApprovePermisionController: NextViewPushDelegate {
     func didTapButton() {
-        let nextViewController = AppSelectViewController()
-        self.navigationController?.pushViewController(nextViewController, animated: false)
+        requestAuthNoti()
+        requestPermission { isScreenTimeApproved in
+            if self.isApproveNoti && isScreenTimeApproved {
+                self.navigationController?.pushViewController(AppSelectViewController(), animated: false)
+            } else if self.isApproveNoti == false {
+                self.view.showToast(message: "알림 권한 설정이 필요해요!", at: 30.adjustedHeight)
+            } else if isScreenTimeApproved == false {
+                self.view.showToast(message: "스크린타임 설정이 필요해요!", at: 30.adjustedHeight)
+            }
+        }
     }
 }
-

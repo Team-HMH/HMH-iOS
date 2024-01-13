ee6d5a71ab389c7a007274116f2ab4e1e418d653
Zoe <Zoe.urssu@gmail.com>
2024-01-14 00:51:00 +0900

    [Fix/#36] Challenge - 목표 시간 설정뷰

--- HMH_iOS/HMH_iOS/Presentation/Challenge/Views/GoalTimeSelectView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Views/GoalTimeSelectView.swift
@@ -14,7 +14,18 @@ import FamilyControls
 class GoalTimeSelectView: UIView {
     
     var screenTime = ScreenTime.shared
-    private let picker = SpecificTimePickerView()
+    private let timePicker = HMHTimePickerView(type: .specificTime)
+    private let timeLable = UILabel().then {
+        $0.text = StringLiteral.Challenge.Time.timeLabel
+        $0.font = .iosText2Medium20
+        $0.textColor = .gray2
+    }
+    private let minPicker = HMHTimePickerView(type: .specificMinute)
+    private let minLable = UILabel().then {
+        $0.text = StringLiteral.Challenge.Time.minLabel
+        $0.font = .iosText2Medium20
+        $0.textColor = .gray2
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,13 +44,30 @@ class GoalTimeSelectView: UIView {
     }
     
     private func setHierarchy() {
-        self.addSubviews(picker)
+        self.addSubviews(timePicker,timeLable,minPicker,minLable)
     }
     
     private func setConstraints() {
-        picker.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.equalTo(335.adjusted)
+        timePicker.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(72.adjusted)
+            $0.width.equalTo(67.adjusted)
+        }
+        
+        timeLable.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(timePicker.snp.trailing).offset(-7.adjusted)
+        }
+        
+        minPicker.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(timeLable.snp.trailing).offset(23.adjusted)
+            $0.width.equalTo(67.adjusted)
+        }
+        
+        minLable.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(minPicker.snp.trailing).offset(2.adjusted)
         }
     }
     

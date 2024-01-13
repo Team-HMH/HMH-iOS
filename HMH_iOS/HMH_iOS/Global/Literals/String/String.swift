02dfbf5cdb5f06e9b28503d6d42cd7cd5131effe
boyeon <judy010119@gmail.com>
2024-01-14 00:22:49 +0900

    [Add/#68] Literals - 이용약관 및 개인정보처리방침 url 추가

--- HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
+++ HMH_iOS/HMH_iOS/Global/Literals/String/String.swift
@@ -67,6 +67,11 @@ enum StringLiteral {
         static var quit = "회원탈퇴"
     }
     
+    enum Setting {
+        static var urlArray = ["https://hmhteam.notion.site/33acb29be57245f394eb93ddb2e3b8cc",
+                               "https://www.notion.so/hmhteam/7006ac1eb36545c38ea2bdfc7e34d2cb?pvs=4"]
+    }
+    
     enum AlertButton {
         static let confirm = "확인"
         static let cancel = "취소"

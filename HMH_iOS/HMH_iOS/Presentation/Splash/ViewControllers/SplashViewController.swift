91d4f3ac4503a3fc46c8ca30e60255957b8e1a47
kim-seonwoo <sw204011@gmail.com>
2024-01-14 01:59:48 +0900

    [Feat/#58] splash - 스플래시 이미지 레이아웃 추가

--- HMH_iOS/HMH_iOS/Presentation/Splash/ViewControllers/SplashViewController.swift
+++ HMH_iOS/HMH_iOS/Presentation/Splash/ViewControllers/SplashViewController.swift
@@ -10,9 +10,35 @@ import Moya
 
 final class SplashViewController: UIViewController {
     
+    private let splashImage = UIImageView().then {
+        $0.image = ImageLiterals.Splash.splash
+        $0.contentMode = .scaleAspectFit
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        setUI()
+        setSplashView()
+    }
+    
+    private func setUI(){
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    private func setViewHierarchy() {
+        view.addSubviews(splashImage)
+    }
+    
+    private func setConstraints() {
+        splashImage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(266.adjusted)
+        }
+    }
+    
+    private func setSplashView() {
+        view.backgroundColor = .background
     }
 }
 

d0ca8bb174ba29af70c03ee7069c78c37e2afbf4
kim-seonwoo <sw204011@gmail.com>
2024-01-14 00:13:39 +0900

    [Fix/#58] Onboarding - SignUpManager로 변경

---
+++ HMH_iOS/HMH_iOS/Presentation/Onboarding/Models/SignUpManager.swift
@@ -0,0 +1,19 @@
+//
+//  SignInModel.swift
+//  HMH_iOS
+//
+//  Created by Seonwoo Kim on 1/9/24.
+//
+
+import UIKit
+
+class SignUpManager {
+    static let shared = SignUpManager()
+    
+    let averageUseTime: String = ""
+    let problem: [String] = []
+    let period: Int = 0
+    let goalTime: Int = 0
+    let appCode: String = ""
+}
+

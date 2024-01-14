d34d04343903fa400fafae6cb8b16a757de2fbb9
kim-seonwoo <sw204011@gmail.com>
2024-01-14 12:59:42 +0900

    [Fix/#73] 코드리뷰 반영

--- HMH_iOS/HMH_iOS/Network/DTO/AuthModel/SocialLoginResponseDTO.swift
+++ HMH_iOS/HMH_iOS/Network/DTO/AuthModel/SocialLoginResponseDTO.swift
@@ -10,9 +10,19 @@ import Foundation
 struct SocialLogineResponseDTO: Codable {
     let userId: Int
     let token: Token
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "userId"
+        case token = "token"
+    }
 }
 
 struct Token: Codable {
     let accessToken: String
     let refreshToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "accessToken"
+        case refreshToken = "refreshToken"
+    }
 }

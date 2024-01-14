447aac98d8f4143520a6029fd8f306e35b38689b
boyeon <judy010119@gmail.com>
2024-01-15 02:22:13 +0900

    [Fix/#81] HomeViewCell - 프로퍼티 리네임

--- HMH_iOS/HMH_iOS/Presentation/Home/Models/TotalAppUsingTimeDataModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Models/TotalAppUsingTimeDataModel.swift
@@ -2,7 +2,15 @@
 //  TotalAppUsingTimeDataModel.swift
 //  HMH_iOS
 //
-//  Created by 김보연 on 1/14/24.
+//  Created by 김보연 on 1/10/24.
 //
 
 import Foundation
+
+struct TotalAppUsingTimeDataModel {
+    var onboardingTotalGoalTime: Float
+    var totalAppUsingTime: Float
+    var progressValue: Float
+    var isFailed: Bool
+}
+

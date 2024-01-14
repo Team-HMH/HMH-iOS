312da3a4e72efa2c7b1d80e62f738d2981743695
boyeon <judy010119@gmail.com>
2024-01-15 00:17:57 +0900

    [Feat/#81] HomeView - 비디오 재생 뷰 추가

--- HMH_iOS/HMH_iOS/Presentation/Home/Views/VideoView.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Views/VideoView.swift
@@ -1,8 +1,32 @@
-//
-//  VideoView.swift
-//  HMH_iOS
-//
-//  Created by 김보연 on 1/14/24.
-//
-
 import Foundation
+import UIKit
+import AVFoundation
+
+class VideoView: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init(videoHandler: VideoHandler) {
+        super.init(frame: .zero)
+        self.avPlayer = videoHandler.avPlayer
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.setPlayerLayer()
+    }
+    
+    private func setPlayerLayer() {
+        self.layer.addSublayer(playerLayer)
+        playerLayer.accessibilityFrame = self.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.frame = self.bounds
+        guard let player = self.avPlayer else { return }
+        playerLayer.player = player
+    }
+    
+    var avPlayer: AVPlayer?
+    private let playerLayer = AVPlayerLayer()
+}

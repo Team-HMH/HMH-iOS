04d8f6f680cf0d98aecdc617386faf140401f941
boyeon <judy010119@gmail.com>
2024-01-15 00:26:35 +0900

    [Feat/#81] HomeViewModel - 비디오 셀 데이터 모델 구축

--- HMH_iOS/HMH_iOS/Presentation/Home/Models/BlackHoleVideoDataModel.swift
+++ HMH_iOS/HMH_iOS/Presentation/Home/Models/BlackHoleVideoDataModel.swift
@@ -2,7 +2,26 @@
 //  BlackHoleVideoDataModel.swift
 //  HMH_iOS
 //
-//  Created by 김보연 on 1/14/24.
+//  Created by 김보연 on 1/10/24.
 //
 
 import Foundation
+import AVFoundation
+import UIKit
+
+struct BlackHoleVideoDataModel {
+    let image: UIImage?
+    let videoItem: URL?
+    let text: String
+}
+
+enum BlackHoleDataType: Int{
+    case firstStep = 0
+    case secondStep = 1
+    case thirdStep = 2
+    case fourthStep = 3
+    case fifthStep = 4
+    case fail = 5
+}
+
+

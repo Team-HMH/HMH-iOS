9c704333c6cf6be7241333b3f28b1f447417b41b
Zoe <Zoe.urssu@gmail.com>
2024-01-16 23:39:17 +0900

    [Feat/#95] 앱 차단 로직

--- HMH_iOS/HMH_iOS/Global/SupportingFiles/AppBlock/SelectedBlocker.swift
+++ HMH_iOS/HMH_iOS/Global/SupportingFiles/AppBlock/SelectedBlocker.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import SwiftUI
+import ManagedSettings
+import DeviceActivity
+import DeviceMonitor
+
+struct SelectedBlocker {
+    
+    let store = ManagedSettingsStore()
+    let model = BlockingApplicationModel.shared
+    
+    // 앱 차단 로직
+    func block(completion: @escaping (Result<Void, Error>) -> Void) {
+        // 선택한 앱 토큰 가져오기
+        let selectedAppTokens = model.selectedAppsTokens
+        
+        // DeviceActivityCenter를 사용하여 모든 선택한 앱 토큰에 대한 액티비티 차단
+        let deviceActivityCenter = DeviceActivityCenter()
+        
+        // 모니터 DeviceActivitySchedule 설정
+        let blockSchedule = DeviceActivitySchedule(
+            intervalStart: DateComponents(hour: 0, minute: 0),
+            intervalEnd: DateComponents(hour: 23, minute: 59),
+            repeats: true
+        )
+        
+        store.shield.applications = selectedAppTokens
+        do {
+            try deviceActivityCenter.startMonitoring(DeviceActivityName.once, during: blockSchedule)
+            
+        } catch {
+            completion(.failure(error))
+            return
+        }
+        completion(.success(()))
+    }
+    
+    func unblockAllApps() {
+        store.shield.applications = []
+    }
+    
+}

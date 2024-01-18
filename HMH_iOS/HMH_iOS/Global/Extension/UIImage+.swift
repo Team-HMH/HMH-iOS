8bfc4bed98a6fdc44126bbdfda913127d5970c6b
boyeon <judy010119@gmail.com>
2024-01-18 23:55:03 +0900

    [Add/#99] Extension - kingFisher 추가

--- HMH_iOS/HMH_iOS/Global/Extension/UIImage+.swift
+++ HMH_iOS/HMH_iOS/Global/Extension/UIImage+.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 extension UIImage {
     static func load(named imageName: String) -> UIImage {
@@ -20,5 +21,20 @@ extension UIImage {
             draw(in: CGRect(origin: .zero, size: size))
         }
         return image
+    }
+}
+
+extension UIImageView {
+    func kfSetImage(url: String?) {
+        
+        guard let url = url else {
+            return }
+        
+        if let url = URL(string: url) {
+            kf.indicatorType = .activity
+            kf.setImage(with: url,
+                        placeholder: nil,
+                        options: [.transition(.fade(1.0))], progressBlock: nil)
+        }
     }
 }

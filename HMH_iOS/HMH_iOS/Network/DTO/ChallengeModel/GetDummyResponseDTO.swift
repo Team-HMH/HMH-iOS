66d59ff10fa4347f569c0e7d39154962e0801a3c
Zoe <Zoe.urssu@gmail.com>
2024-01-20 01:42:31 +0900

    [Fix/#123] 주석 제거

--- HMH_iOS/HMH_iOS/Network/DTO/ChallengeModel/GetDummyResponseDTO.swift
+++ HMH_iOS/HMH_iOS/Network/DTO/ChallengeModel/GetDummyResponseDTO.swift
@@ -22,20 +22,3 @@ struct App: Codable {
         case goalTime, usageTime
     }
 }
-//
-//var getDummyResponseDTO: [GetDummyResponseDTO] = [
-//    .init(apps: [App(appName: "Netflix",
-//                     appImageURL: "https://github.com/Team-HMH/HMH-Server/assets/69035864/dd068b83-641a-4bff-b4f5-381ea6e04d44",
-//                     goalTime: 2400000,
-//                     usageTime: 1320000)]),
-//    
-//    .init(apps: [App(appName: "Instagram",
-//                     appImageURL: "https://github.com/Team-HMH/HMH-Server/assets/69035864/bd572377-9dd9-47e7-a9f4-9efbbe66cd2e",
-//                     goalTime: 5400000,
-//                     usageTime: 2880000)]),
-//    
-//    .init(apps: [App(appName: "YouTube",
-//                     appImageURL: "https://github.com/Team-HMH/HMH-Server/assets/69035864/8afa60c0-bf1d-4ff0-9b50-4d1558a71f0a",
-//                     goalTime: 3300000,
-//                     usageTime: 900000)]),
-//]

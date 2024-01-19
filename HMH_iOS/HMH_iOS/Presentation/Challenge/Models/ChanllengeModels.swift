4d2eb98a8381eee514eec2b6522f692cfbf9b3d8
Zoe <Zoe.urssu@gmail.com>
2024-01-20 04:38:54 +0900

    [Fix/#123] 챌린지뷰 UI 수정

--- HMH_iOS/HMH_iOS/Presentation/Challenge/Models/ChanllengeModels.swift
+++ HMH_iOS/HMH_iOS/Presentation/Challenge/Models/ChanllengeModels.swift
@@ -17,11 +17,27 @@ final class ChallengeManager {
     static let shared = ChallengeManager()
     private init() {
         self.type = .completed
+        loadChallenge()
     }
     var type: ChallengeType
     
     func updateType(_ type: ChallengeType) {
         self.type = type
+    }
+    
+    func loadChallenge() {
+        let provider = Providers.challengeProvider
+        provider.request(target: .getChallenge, instance: BaseResponse<GetChallengeResponseDTO>.self) { result in
+            if let data = result.data {
+               if data.todayIndex == -1 {
+                    self.type = .completed
+               } else if data.period == 7 {
+                   self.type = .sevenDays
+               } else if data.period == 14 {
+                   self.type = .fourteenDays
+               }
+            }
+        }
     }
 }
 
